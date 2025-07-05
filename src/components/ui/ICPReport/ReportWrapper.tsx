918258836b076a986063dd0b09eeaea0e6ea8ed4
Zachary Chester <zach.chester@gmail.com>
2025-07-05 11:00:21 -0500

    fix: use correct relative imports for modular report section components

--- src/components/ui/ICPReport/ReportWrapper.tsx
+++ src/components/ui/ICPReport/ReportWrapper.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ExecutiveSummary from "./ExecutiveSummary";
-import CompanyOverview from "./CompanyOverview";
-import MarketIntelligence from "./MarketIntelligence";
-import ICPIBPFramework from "./ICPIBPFramework";
-import SalesGTMStrategy from "./SalesGTMStrategy";
-import TechnologyStack from "./TechnologyStack";
+import ExecutiveSummary from './ExecutiveSummary';
+import CompanyOverview from './CompanyOverview';
+import MarketIntelligence from './MarketIntelligence';
+import ICPIBPFramework from './ICPIBPFramework';
+import SalesGTMStrategy from './SalesGTMStrategy';
+import TechnologyStack from './TechnologyStack';
 
 interface ReportWrapperProps {
   reportData: any;
@@ -16,14 +16,17 @@ export default function ReportWrapper({ reportData }: ReportWrapperProps) {
     ? JSON.parse(reportData.llm_output) 
     : reportData.llm_output || reportData;
 
+  // If the modular structure exists, use it; otherwise, fallback to flat
+  const modular = data.merged || data;
+
   return (
     <div className="max-w-5xl mx-auto p-6 space-y-8">
-      <ExecutiveSummary data={data} />
-      <CompanyOverview data={data} />
-      <MarketIntelligence data={data} />
-      <ICPIBPFramework data={data} />
-      <SalesGTMStrategy data={data} />
-      <TechnologyStack data={data} />
+      <ExecutiveSummary data={modular.executiveSummary || modular} />
+      <CompanyOverview data={modular.companyOverview || modular} />
+      <MarketIntelligence data={modular.marketIntelligence || modular} />
+      <ICPIBPFramework data={modular.icpIbps || modular} />
+      <SalesGTMStrategy data={modular.salesGtmStrategy || modular} />
+      <TechnologyStack data={modular.technologyStack || modular} />
     </div>
   );
 } 
