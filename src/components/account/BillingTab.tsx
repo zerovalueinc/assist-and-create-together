5fcb1e10de38a442fb8716ef235f6e517d95ec60
Zachary Chester <zach.chester@gmail.com>
2025-07-06 16:31:00 -0500

    fix: Add 'use client' to all account tab components for Next.js app directory compatibility

--- src/components/account/BillingTab.tsx
+++ src/components/account/BillingTab.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
